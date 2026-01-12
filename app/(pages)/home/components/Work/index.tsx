24b4b7594b352378518dabdc86db62fc3afc5feb
Nguyen Anh Nhat <nhat.nguyen@pigroup.com.vn>
2026-01-12 23:46:44 +0700

    feat: work content

--- app/(pages)/home/components/Work/index.tsx
+++ app/(pages)/home/components/Work/index.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
 import "../../styles/work-component.scss";
 
-type Props = {};
-
-const WorkComponent = (props: Props) => {
-  return <div className="work-container">WorkComponent</div>;
+const WorkComponent = () => {
+  return (
+    <div className="work-container">
+      <div className="work-content">
+        <Image
+          className="work-content__bg-paper"
+          src={"/images/bg-paper.jpg"}
+          alt="work-1"
+          quality={100}
+          width={1200}
+          height={1200}
+        />
+        <div className="work-content__info">
+          <h1 className="work-content__info-title">
+            Translating complex protocols into functional interfaces. Focused on
+            utility, structure, and user autonomy.
+          </h1>
+          <div className="work-content__info-tags">
+            <div className="offer">
+              <span className="offer__text">OPEN TO:</span>
+              <span className="offer__value">
+                OFFERS<span className="offer__value-blinker">_</span>
+              </span>
+            </div>
+            <div className="progress">
+              <div className="progress__bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="work-list">
+        <div className="work-list__item"></div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkComponent;

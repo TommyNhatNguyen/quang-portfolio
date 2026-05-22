014f22207ea0cd15a243454e5d0b43d3d33d5e9f
Nguyen Anh Nhat <nhat.nguyen@pigroup.com.vn>
2026-05-22 10:47:05 +0700

    feat: folder tab

--- next.config.ts
+++ next.config.ts
@@ -1,64 +1,39 @@
 import type { NextConfig } from "next";
 
-// const remotePatterns: any[] = [];
+const remotePatterns: any[] = [];
 
-// if (process.env.NODE_ENV === "development") {
-//   remotePatterns.push({
-//     protocol: "http",
-//     hostname: "localhost",
-//     port: "1340",
-//     pathname: "/api/**",
-//   });
-//   remotePatterns.push({
-//     protocol: "http",
-//     hostname: "0.0.0.0",
-//     port: "1340",
-//     pathname: "/uploads/**",
-//   });
-// } else {
-//   // remotePatterns.push({
-//   //   protocol: "https",
-//   //   hostname: "dqharchitects.vn",
-//   //   port: "",
-//   //   pathname: "/**",
-//   // });
-//   // remotePatterns.push({
-//   //   protocol: "https",
-//   //   hostname: "cms.dqharchitects.vn",
-//   //   port: "",
-//   //   pathname: "/**",
-//   // });
-// }
+if (process.env.NODE_ENV === "development") {
+  remotePatterns.push({
+    protocol: "http",
+    hostname: "localhost",
+    port: "1340",
+    pathname: "/api/**",
+  });
+  remotePatterns.push({
+    protocol: "http",
+    hostname: "0.0.0.0",
+    port: "1340",
+    pathname: "/uploads/**",
+  });
+} else {
+  remotePatterns.push({
+    protocol: "https",
+    hostname: "quanglaam.com",
+    port: "",
+    pathname: "/**",
+  });
+  remotePatterns.push({
+    protocol: "https",
+    hostname: "cms.quanglaam.com",
+    port: "",
+    pathname: "/**",
+  });
+}
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     unoptimized: process.env.NODE_ENV === "development",
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1340",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1340",
-        pathname: "/api/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1340",
-        pathname: "/**/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1340",
-        pathname: "/**",
-      },
-    ],
+    remotePatterns,
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -71,7 +46,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: `/home`,
+        destination: `/work`,
         permanent: false,
       },
     ];
